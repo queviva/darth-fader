1c801082085a202ee36df2a243a5ee1bb68d26fd
queviva <jabraham.engineer@gmail.com>
2021-12-28 19:00:46 -0800

    want a backup now of a not-yet-complete rewrite

--- darth-faderScript.js
+++ darth-faderScript.js
@@ -1,12 +1,20 @@
+////////////////////////////////////////////////////////////
+// pizzaface - MCMLXXXVIII
+////////////////////////////////////////////////////////////
 
+// content loaded new func closure
 
-// window load new func closure
-window.addEventListener('load', () => { new (function () {
+/*
+((dset = document.currentScript.dataset) =>
+
+document.addEventListener('DOMContentLoaded', () => new (function () {
+
+    console.log(dset);
 
     // list of default preferences
-    const defPrefs = {
-        selector: 'darthfader',
-        faderClassName: 'darth-fader',
+    const default_Prefs = {
+        appName: 'darth-fader',
+        selector: 'darth',
         loadedEventName: 'darthloaded',
         inputEventName: 'darthfaded',
         fix: false,
@@ -24,19 +32,21 @@ window.addEventListener('load', () => { new (function () {
         able: true
     };
 
-    // grab any opts set in data-param of the script tag
-    const opts = JSON.parse(((document.querySelector(
-        'script[src*="darth-fader"][src$=".js"]'
-    ) || {}).dataset || {}).darth || '{}');
+    // grab prefs set in the data-parameter of the script-tag
+    const param_prefs = JSON.parse(Object.values(dset)[0] || '{}');
 
-    // over rite any valid default prefs passed in
-    for (let p in opts) {
-        if (defPrefs[p] !== undefined) defPrefs[p] = opts[p];
-    }
-
+    // grab any stored prefs
+    const stored_prefs = JSON.parse( localStorage[
+            param_prefs.appname || default_prefs.appname
+    ] || '{}');
+    
+    // assemble prefs by overwriting eachother
+    const prefs = Object.assign({}, default_prefs, param_prefs, stored_prefs);
+    
     // correct any typos
-    defPrefs.faderClassName = defPrefs.faderClassName.replace(/[^a-z-]/gi,'');
-        
+    // DEBUGG - might impliment this
+    //defPrefs.faderClassName.replace(/[^a-z-]/gi, '');
+
     // method to parse a JSON formated fade
     const parseFade = fade => {
 
@@ -101,95 +111,95 @@ window.addEventListener('load', () => { new (function () {
 
     };
 
-    // the dart fader object
+    // the darth fader object
     const DarthFader = function(obj) {
-    
+
         // expose preferences for this obj
         this.prefs = {};
-    
+
         // parse any prefs included in the input tag's data-params
         let dataPrefs = obj.dataset.darthfader ? JSON.parse(obj.dataset.darthfader) : {};
-    
+
         // if any valid prefs were given, set them
         for (let p in defPrefs) {
             this.prefs[p] = dataPrefs[p] !== undefined ? dataPrefs[p] : defPrefs[p];
         }
-    
+
         // reference to the html element
         this.obj = obj;
-    
+
         // for exposing all the values
         this.vals = {};
-    
+
         // get all the fade list data from the html
         let fadeList = this.prefs.fades;
-    
+
         // parse the fades data
         for (let fade in fadeList) {
-    
+
             // send it through the parser
             fadeList[fade] = parseFade(fadeList[fade]);
-    
+
             // create a hidden input for this fade
             let iPut = document.createElement('input');
             iPut.type = 'hidden';
-    
+
             // give the input a name and id
             iPut.id = iPut.name = `${obj.id}_${fade}`;
-    
+
             // add it to the fader and hidden inputs list
             //obj.appendChild(this.hiddenInputs[fade] = iPut);
             obj.appendChild(iPut);
-    
+
         }
-        
+
         // check if snap option set in datalist
         if (this.prefs.snap && fadeList[this.prefs.snap]) {
-    
+
             // create a shorthand
             let fad = fadeList[this.prefs.snap],
-    
+
                 // create a list
                 tmpL = document.createElement('datalist');
-    
+
             // loop over the specified fade
             for (let i = 0, j = fad.length; i < j; i += 2) {
-    
+
                 // add an option for this point
                 let tmpO = document.createElement('option');
                 tmpO.value = fad[i + 1];
                 tmpL.appendChild(tmpO);
-    
+
             }
-    
+
             // add the list to the input element
             obj.appendChild(tmpL);
-    
+
             // give this obj the right list
             // ALERT ::: has to be by _ID_!
             tmpL.id = `${obj.id}_datalist`;
             obj.setAttribute('list', tmpL.id);
-    
+
         }
-    
+
         // check if color-the-thumb option set
         if (this.prefs.thumb) {
-    
+
             // check if the specified fade exists
             if (fadeList[this.prefs.thumb]) {
-    
+
                 // add a listener
                 obj.addEventListener(this.prefs.inputEventName, e => {
-    
+
                     // force the color back on itself
                     obj.style.setProperty('--thumb-color',
                         this.vals[this.prefs.thumb]);
-    
+
                 });
-    
+
             }
             else { // other wize need to create colors
-    
+
                 // send them through the parser
                 let privateVarnish = parseFade(
                     [
@@ -198,13 +208,13 @@ window.addEventListener('load', () => { new (function () {
                         'rgb(238,170,0)', 1
                     ]
                 );
-    
+
                 // listen for values changed
                 obj.addEventListener(this.prefs.inputEventName, e => {
-    
+
                     let tmp = {},
                         objV = parseFloat(obj.value);
-    
+
                     // figure out who's on top
                     if (objV < 0.5) {
                         tmp.lowEnd = privateVarnish[0];
@@ -218,25 +228,25 @@ window.addEventListener('load', () => { new (function () {
                         tmp.topEnd = privateVarnish[4];
                         tmp.topStop = 1;
                     }
-    
+
                     // expose this val to others
                     this.vals[this.prefs.thumb] = calcFade(tmp, objV, '0');
-    
+
                     // set the thumb color from the calculated val
                     obj.style.setProperty('--thumb-color', calcFade(tmp, objV, '0'));
-    
+
                 });
             }
         }
-    
+
         // set an initial value
         obj.value = this.prefs.value;
-    
+
         // listen for inputs to the slider
         obj.addEventListener('input', e => {
             this.inputHandler(this, fadeList);
         });
-    
+
     };
 
     // proto to trigger load and input events on load, passes reference
@@ -247,63 +257,62 @@ window.addEventListener('load', () => { new (function () {
                     ref: this
                 },
                 input: this.obj.dispatchEvent(new Event('input')),
-            }
-        ));
+            }));
     };
 
     // method to handle the input event from the slider
     DarthFader.prototype.inputHandler = function(DF, fadeList) {
-        
+
         // create a float number of the current value
         let slyVal = parseFloat(DF.obj.value);
-    
+
         // loop through all of the fades in the list
         for (let fade in fadeList) {
-    
+
             // abbreviate the current fade
             let curFade = fadeList[fade];
-    
+
             // needs a counter
             let cnt = 0;
-    
+
             // compute the low end array and its stop
             while (
-    
+
                 // if this fade's stop is less than the value
                 curFade[(1 + cnt * 2)] <= slyVal &&
                 // and not the 100% stop
                 curFade[(1 + cnt * 2)] != 1
-    
+
             ) {
-    
+
                 // set the low end array and its stop
                 curFade.lowStop = curFade[1 + cnt * 2];
                 curFade.lowEnd = curFade[cnt * 2];
                 cnt++;
-    
+
             }
-    
+
             // set the top end array and its stop
             curFade.topStop = curFade[1 + 2 * cnt];
             curFade.topEnd = curFade[2 * cnt];
-    
+
             // finally create the vals for this fade
             DF.vals[fade] =
-    
-            // by calling the calc
-            calcFade(curFade, slyVal, DF.prefs.fix);
-    
+
+                // by calling the calc
+                calcFade(curFade, slyVal, DF.prefs.fix);
+
         }
-    
+
         // let any listeners know
         DF.obj.dispatchEvent(new CustomEvent(
             DF.prefs.inputEventName, {
                 detail: { vals: DF.vals }
             }
         ));
-    
+
     };
-    
+
     // exposed ability to set value
     DarthFader.prototype.setValue = function(v) {
         this.obj.value = v;
@@ -317,23 +326,23 @@ window.addEventListener('load', () => { new (function () {
     document.querySelectorAll(
         `input[data-${ defPrefs.selector.replace(/[^a-z]/gi,'') }]`
     ).forEach(obj => {
-                    
+
         // configure the input element
         obj.type = 'range';
         obj.min = 0;
         obj.max = 1;
         obj.step = 0.001;
-        
+
         // set the classname
         obj.classList.add(defPrefs.faderClassName)
-        
+
         obj.style.setProperty(
             '--thumb-height',
             window.getComputedStyle(obj).height
         );
-        
+
         allDarths[obj.id] = new DarthFader(obj);
-        
+
     });
 
     // add the styles needed for the inputs
@@ -371,11 +380,11 @@ window.addEventListener('load', () => { new (function () {
     input.${defPrefs.faderClassName}[type=range]:focus { outline: none; }
     
 } `;
-     //}
+    //}
 
     // set the thumb height on resize
     window.addEventListener('resize', e => {
-    
+
         for (let D in allDarths) {
 
             allDarths[D].obj.style.setProperty(
@@ -384,7 +393,7 @@ window.addEventListener('load', () => { new (function () {
             );
 
         }
-    
+
     });
 
     // let any listeners know this is loaded
@@ -395,4 +404,8 @@ window.addEventListener('load', () => { new (function () {
         allDarths[obj].loadComplete();
     }
 
-})()});
+})()
+    
+))();
+
+*/
